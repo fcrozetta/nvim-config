ecdb825ac29e786f45bdddfc46307c3a51bb2d32
fcrozetta <8006246+fcrozetta@users.noreply.github.com>
2026-04-09 15:43:10 +0200

    we don't check if brew exist, when we are installing with brew install

--- setup.sh
+++ setup.sh
@@ -9,17 +9,8 @@ echo "==> Neovim config setup"
 
 # --- Install mode: skip brew deps if called from formula ---
 if [ "${HOMEBREW_FORMULA:-}" != "1" ]; then
-
-  # --- Homebrew ---
-  if ! command -v brew &>/dev/null; then
-    echo "==> Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  fi
-
   echo "==> Installing brew packages..."
   xargs brew install < "$DEPS_DIR/brew.txt"
-
 fi
 
 # --- Symlink config ---
